2fa12402a810fec83d99c17ca281ef37bd9d49e3
Geert van Horrik <geert@catenalogic.com>
2018-04-11 11:57:08 +0200

    Trying a fix for onovotny/MSBuildSdkExtras/issues/43

--- src/Ghk.MultiTargeting.Example.Wpf/MainWindow.xaml.cs
+++ src/Ghk.MultiTargeting.Example.Wpf/MainWindow.xaml.cs
@@ -9,11 +9,11 @@ namespace Ghk.MultiTargeting.Example
 {
     using System.Windows;
 
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
